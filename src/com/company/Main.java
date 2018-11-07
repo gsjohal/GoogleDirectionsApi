bbcb41e6fec26ed1fb1adb55677627a05ff9e2fd
Gurmeet <gsjohal@edu.uwaterloo.ca>
2018-11-06 20:31:59 -0500

    final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 import com.google.gson.*;
 
-import static jdk.nashorn.internal.runtime.regexp.joni.Syntax.Java;
+
 
 public class Main {
 
@@ -28,26 +28,31 @@ public class Main {
 
         JsonObject jsonData = DirectionApi.getRoutes(origin, dest, mode, avoid);
 
+
         try{
             JsonArray routes = jsonData.getAsJsonArray("routes");
+            if (routes.size() > 0){
+                JsonObject routeObject = routes.get(0).getAsJsonObject();
 
-            JsonObject routeObject = routes.get(0).getAsJsonObject();
+                JsonArray legs = routeObject.getAsJsonArray("legs");
 
-            JsonArray legs = routeObject.getAsJsonArray("legs");
+                JsonObject stepsObject = legs.get(0).getAsJsonObject();
 
-            JsonObject stepsObject = legs.get(0).getAsJsonObject();
+                JsonArray stepsArray = stepsObject.getAsJsonArray("steps");
 
-            JsonArray stepsArray = stepsObject.getAsJsonArray("steps");
-
-            for(int i = 0; i < stepsArray.size(); i++ ){
-                JsonObject stepsElement = stepsArray.get(i).getAsJsonObject();
-                String distance = removeQuotes(stepsElement.get("distance").getAsJsonObject().get("text").toString());
-                String direction = removeQuotes(stepsElement.get("html_instructions").toString());
-                direction = direction.replaceAll("\\<[^>]*>","");
-                String duration = removeQuotes(stepsElement.get("duration").getAsJsonObject().get("text").toString());
-                System.out.println("Step "+(i+1)+":");
-                System.out.println("In "+distance+", "+direction+". This should take you "+duration+".\n");
+                for(int i = 0; i < stepsArray.size(); i++ ){
+                    JsonObject stepsElement = stepsArray.get(i).getAsJsonObject();
+                    String distance = removeQuotes(stepsElement.get("distance").getAsJsonObject().get("text").toString());
+                    String direction = removeQuotes(stepsElement.get("html_instructions").toString());
+                    direction = direction.replaceAll("\\<[^>]*>","");
+                    String duration = removeQuotes(stepsElement.get("duration").getAsJsonObject().get("text").toString());
+                    System.out.println("Step "+(i+1)+":");
+                    System.out.println("In "+distance+", "+direction+". This should take you "+duration+".\n");
+                }
+            } else {
+                System.out.println("Invalid input origin or destination");
             }
+
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -55,7 +60,7 @@ public class Main {
 
     }
 
-    public static String removeQuotes(String str){
-        return str.replaceAll("^\"+ \"+$","");
+    private static String removeQuotes(String str){
+        return str.replaceAll("\"","");
     }
 }
